7cff206b853967be51b9a79b34e67d99f5b7d1a1
ibnatysalsabila <salsajpr01@gmail.com>
2025-10-24 16:01:01 +0700

    menyusun posisi dan padding text di bagian bawah tengah

--- app/src/main/java/com/example/questadvancelayout_047/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_047/UIPertama.kt
@@ -92,3 +92,10 @@ fun ActivitasPertama(modifier: Modifier){
         ){
             Text(
                 text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+}
